f158a6f8a94dae0f3ee18df53029a0e457c185b6
abhinandanmishra1 <abhinandanmishra360@gmail.com>
2023-04-02 23:49:25 +0530

    feat: backend deployed

---
+++ README.md
@@ -0,0 +1,29 @@
+## MERN Blog Site - [Live Version](#mern-blog-site---live-version)
+
+### Tech Stack
+- React with Typescript ( Frontend )
+- Node.js and express ( Backend )
+- MongoDb ( Database )
+- Styling is done using -> Tailwind CSS 
+
+### Deployments
+- Backend Service is deployed on [render](http://onrender.com/)
+- Frontend Application is deployed on [vercel](https://vercel.com/)
+
+### Features 
+- Create Blog
+- Update Blog
+- Delete Blog
+- Get Blogs
+- Get Blog
+
+### Blog Schema
+```js
+Blog {
+    title: string,
+    description: string,
+    image: string
+}
+
+// image has been stored as base64 string
+```
--- src/api/index.tsx
+++ src/api/index.tsx
@@ -2,21 +2,21 @@ import axios from "axios";
 import { Blog } from "../Blog.type";
 
 export const getBlogs = async () => {
-  return await axios.get("http://localhost:5000/api/blogs");
+  return await axios.get("https://blog-0szz.onrender.com/api/blogs");
 };
 
 export const getBlog = async (id: string) => {
-  return await axios.get(`http://localhost:5000/api/blogs/${id}`);
+  return await axios.get(`https://blog-0szz.onrender.com/api/blogs/${id}`);
 };
 
 export const createBlog = async (blog: Blog) => {
-  return await axios.post("http://localhost:5000/api/blogs", blog);
+  return await axios.post("https://blog-0szz.onrender.com/api/blogs", blog);
 };
 
 export const updateBlog = async (id: string, blog: Blog) => {
-  return await axios.put(`http://localhost:5000/api/blogs/${id}`, blog);
+  return await axios.put(`https://blog-0szz.onrender.com/api/blogs/${id}`, blog);
 };
 
 export const deleteBlog = async (id: string) => {
-  return await axios.delete(`http://localhost:5000/api/blogs/${id}`);
+  return await axios.delete(`https://blog-0szz.onrender.com/api/blogs/${id}`);
 };
